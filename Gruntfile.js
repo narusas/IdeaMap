7b56597d5190b634a8fd433132588d5fcaf5d0cd
narusas <narusas@gmail.com>
2014-03-08 18:35:53 +0900

    grunt requirejs적용

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         separator: ';'
       },
       dist: {
-        src: ['src/**/*.js'],
+        src: ['assets/**/*.js'],
         dest: 'dist/<%= pkg.name %>.js'
       }
     },
@@ -50,7 +50,26 @@ module.exports = function(grunt) {
         rjsConfig: 'test/config.js',
         baseUrl: 'assets'
       }
-    }
+    },
+    requirejs: {
+      prototype2: { // 모듈명임, 임의의 이름을 쓸수 있음
+        options: {
+          baseUrl: "assets/",
+          name: "prototype2",
+          mainConfigFile: "assets/config.js",
+          out: "dist/assets/prototype2.js",
+          optimize:'none'
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['*.html'], dest: 'dist/', filter: 'isFile'},
+          {expand: true, src: ['bower_components/**'], dest: 'dist/',filter: 'isFile'},
+        ]
+      }
+    },
 
   });
 
@@ -59,14 +78,19 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+
+  //Optimize RequireJS projects using r.js.
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+
   //grunt.loadNpmTasks('grunt-bower-task');  //  grunt-bower-requirejs 와 충돌 함 
+
+  //Automagically wire-up installed Bower components into your RequireJS config
   grunt.loadNpmTasks('grunt-bower-requirejs');
-  
 
-
-  
   grunt.registerTask('test', ['jshint', 'qunit']);
 
-  grunt.registerTask('default', ['bower', 'jshint', 'qunit', 'concat']);
+  grunt.registerTask('default', ['bower', 'jshint', 'qunit', 'requirejs', 'copy']);
 
 };
